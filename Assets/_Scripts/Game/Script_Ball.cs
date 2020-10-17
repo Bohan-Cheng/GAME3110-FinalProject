f26a36caf97cec16f2a738a5d25a015efb96b9ca
DepressedCookie <vipc.1224@live.ca>
2020-10-17 18:07:46 -0400

    Added Ball prefab and Logic, Particles

---
+++ Assets/_Scripts/Game/Script_Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Ball : MonoBehaviour
+{
+    public ParticleSystem ImpactPS;
+
+    [SerializeField] float BallSpeed = 15.0f;
+    [SerializeField] float MaxSpeed = 15.0f;
+    Vector3 Direction = Vector3.zero;
+    Rigidbody rigi;
+
+    void RandomDirection()
+    {
+        Direction = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigi = gameObject.GetComponent<Rigidbody>();
+        RandomDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigi.AddForce(Direction * BallSpeed * Time.deltaTime);
+
+        // Limit max speed of the player
+        rigi.velocity = Vector3.ClampMagnitude(rigi.velocity, MaxSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ImpactPS.gameObject.SetActive(true);
+        ImpactPS.Play();
+        
+        if (collision.gameObject.tag == "Player")
+        {
+            Direction = collision.contacts[0].normal;
+        }
+        else
+        {
+            Direction = Vector3.Reflect(Direction, collision.contacts[0].normal);
+        }
+            rigi.velocity = Vector3.zero;
+    }
+}
