9f3a0674883566dd89a52022a54e3c61f346f8ba
DepressedCookie <vipc.1224@live.ca>
2020-10-17 00:32:38 -0400

    Added player control script

---
+++ Assets/_Scripts/Game/Script_PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_PlayerControl : MonoBehaviour
+{
+    public bool IsHost = true;
+
+    float MoveSpeed = 5.0f;
+    float MaxSpeed = 10;
+    Rigidbody rigi;
+
+    void Start()
+    {
+        if (!IsHost)
+            MoveSpeed *= -1;
+        rigi = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        // Move the player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rigi.AddForce(movement * MoveSpeed);
+
+        // Limit max speed of the player
+        rigi.velocity = Vector3.ClampMagnitude(rigi.velocity, MaxSpeed);
+
+        // Slow stop the player
+        if (moveHorizontal == 0)
+            rigi.velocity = Vector3.Lerp(rigi.velocity, Vector3.zero, 0.02f);
+    }
+}
