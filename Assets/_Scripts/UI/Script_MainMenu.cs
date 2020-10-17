e34744f63155355a74a8b7aa02090851774d1cc7
DepressedCookie <vipc.1224@live.ca>
2020-10-16 22:27:29 -0400

    Initial Project

---
+++ Assets/_Scripts/UI/Script_MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_MainMenu : MonoBehaviour
+{
+    public string StartMap;
+
+
+    // Go to the game map
+    public void OnStartPressed()
+    {
+        SceneManager.LoadScene(StartMap);
+    }
+
+    // Quit the game
+    public void OnExitPressed()
+    {
+        Application.Quit();
+    }
+}
