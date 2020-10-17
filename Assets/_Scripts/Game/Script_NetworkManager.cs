f831f9c1c0a53e7075af5ab7c97cd12191f75ae6
DepressedCookie <vipc.1224@live.ca>
2020-10-17 00:32:23 -0400

    Added NetworkMana script

---
+++ Assets/_Scripts/Game/Script_NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_NetworkManager : MonoBehaviour
+{
+    public GameObject Cam1;
+    public GameObject Cam2;
+
+    public GameObject Player1;
+    public GameObject Player2;
+
+
+    void ActivatePlayer(bool host)
+    {
+        if (host)
+        {
+            Cam1.SetActive(true);
+            Player1.AddComponent<Script_PlayerControl>().IsHost = host;
+        }
+        else
+        {
+            Cam2.SetActive(true);
+            Player2.AddComponent<Script_PlayerControl>().IsHost = host;
+        }
+    }
+
+
+    void Start()
+    {
+        ActivatePlayer(true);
+    }
+}
