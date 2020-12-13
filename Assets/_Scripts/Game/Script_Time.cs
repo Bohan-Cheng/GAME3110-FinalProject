161e42169c66857987d59f7a9738741ec39c4b2e
VergilZh <z1205067147@gmail.com>
2020-12-12 20:43:13 -0500

    Add time countdown

---
+++ Assets/_Scripts/Game/Script_Time.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Script_Time : MonoBehaviour
+{
+    public int GameTime;
+    public TextMeshProUGUI ShowGameTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameTime = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(GameTime > 0)
+        {
+            StartCoroutine(CountDown());
+        }
+        ShowGameTime.text = GameTime.ToString();
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GameTime --;
+    }
+}
